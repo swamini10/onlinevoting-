7dd9393990e8fb74938e2de6714c5aa6f6a17653
swamini10 <swaminibhandare10@gmail.com>
2025-08-18 18:32:14 +0530

    PostMapping is done

--- src/main/java/com/onlinevoting/controller/UserDetailsController.java
+++ src/main/java/com/onlinevoting/controller/UserDetailsController.java
@@ -1,13 +1,13 @@
 package com.onlinevoting.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserDetailsController {
 
-    @GetMapping("/v1/data")
+    @PostMapping("/v1/data")
     public String getData(){
-        return "sss";
+        return "swamini";
     }
 }
